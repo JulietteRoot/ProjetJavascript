3f127efafd1c009a3f84accac4844426b7324277
greta <greta@lasco>
2013-05-14 12:21:28 +0200

    validation du formulaire

--- index.php
+++ index.php
@@ -6,10 +6,13 @@
     </head>
     <body>
         <h1 id="result"></h1>
-        Nombre 1 : <input type="text" name="nb1" id="nb1"><br>
-        Nombre 2 : <input type="text" name="nb2" id="nb2"><br>
-        <!-- <input type="button" value="push" onclick="alert('ici');"> -->
-        <input type="button" value="push" onclick="additionner();">
+        <form action="#" id="myform" name="myform">
+        <!-- la méthode est en GET par défaut -->
+            Nombre 1 : <input type="text" name="nb1" id="nb1"><br>
+            Nombre 2 : <input type="text" name="nb2" id="nb2"><br>
+            <!-- <input type="button" value="push" onclick="alert('ici');"> -->
+            <input type="button" value="push" onclick="additionner();">
+        </form>
         <script type="text/javascript" src="js/script.js"></script>
     </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -15,8 +15,46 @@
 //affiche(truc);
 //var nb1Element = document.getElementById("nb1");
 function additionner(){
-    var nb1 = document.getElementById("nb1").value; // on récupère directement sa valeur
-    var nb2 = document.getElementById("nb2").value;
-    var somme = nb1 + nb2; // comme cela, ça concatène les chiffres, ne les additionne pas.
-    console.log(somme);
+    var messages = new Array();
+    var valid = true;
+    // on récupère directement sa valeur :
+    // var nb1 = document.getElementById("nb1").value;
+    // var nb2 = document.getElementById("nb2").value;
+    // Ou :
+    var nb1 = document.myform.nb1.value;
+    if(! /^\d+$/.test(nb1)){ // \d revient à [0-9]
+        messages.push("Le premier nombre est incorrect");
+        // push pour ajouter à la fin du tableau
+        valid = false;
+    }
+    // \d revient à [0-9]
+    var nb2 = document.myform.nb2.value;
+    if(! /^\d+$/.test(nb2)){ // \d revient à [0-9]
+        messages.push("Le deuxième nombre est incorrect");
+        valid = false;
+    }
+    var resultElement = document.getElementById("result");
+    resultElement.innerHTML = "";
+    // nettoie la place avant d'écrire autre chose, on vide ce qui était présent.
+    // sans cela, les précédents messages restent inscrits.
+    if(valid){
+        //var somme = nb1 + nb2;
+        // comme cela, ça concatène les chiffres, ne les additionne pas.
+        var somme = parseInt(nb1) + parseInt(nb2);
+        // fonctionne, les nbs sont considérés maintenant comme des nombres
+        // et non des chaînes de caractères.
+        resultElement.innerHTML =
+                nb1 +" + "+nb2+" = "+somme;
+        //var somme = Number(nb1) + Number(nb2); // fonctionnerait aussi
+        //var somme = parseInt(nb1) + parseInt('azerty'); // renvoie NaN
+        //console.log(somme);
+        //document.getElementById("result").innerHTML = nb1 +" + "+nb2+" = "+somme;
+        
+    }else {
+        for (var i = 0 ; i < messages.length ; i++){
+            resultElement.innerHTML=
+            resultElement.innerHTML+
+            messages[i];
+        }
+    }
 }
