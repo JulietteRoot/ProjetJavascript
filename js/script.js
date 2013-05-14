3bf46b51fba80c38f576b2baa59921e577f7f849
greta <greta@lasco>
2013-05-14 10:21:34 +0200

    premier test

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 //alert('coucou');
 var truc = "machin";
-alert(truc);
+//alert(truc);
+for(var i = 0 ; i < 10 ; i++){
+    // on utilise un objet console, et on log le résultat de i
+    console.log(i);
+    // c'est dans la console JS qu'on la verra
+}
 
 
